57bf3421727395e5a6d1aa05ae2a4f274e692ae1
MnbYuk <haiiroorennji@gmail.com>
2024-03-17 06:39:11 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+    get 'top' => 'homes#top'
+    get 'edit/:id' => 'books#edit'
+    resources :books
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# get 'top' => 'homes#top'
 end
