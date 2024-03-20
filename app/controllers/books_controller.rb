2e823d3470bc0622399fe7ed1ba8640704b1ad4b
MnbYuk <haiiroorennji@gmail.com>
2024-03-20 11:54:23 +0000

    resources実装前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book.id)
     else
       flash.now[:alert] = "失敗"
-      render :new
+      render :books
     end
   end
 
@@ -36,7 +36,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to index_book_path
+    redirect_to book_path
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-    get 'new' => 'books#new'
-    post 'books' => 'books#create'
     get '/' => 'homes#top'
-    get 'books' => 'books#index', as: 'index_book'
-    patch 'books/:id' =>'books#update', as: 'update_book'
-    get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-    get 'books/:id' => 'books#show', as: 'book'
-    delete 'books' => 'books#destroy', as: 'destroy_book'
-    # get 'edit/:id' => 'books#edit'
-    # resources :books
+    resources :books
+
+    # post 'books' => 'books#create'
+    # patch 'books/:id' =>'books#update', as: 'update_book'
+    # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+    # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+    # get 'books/:id' => 'books#show', as: 'book'    #
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
